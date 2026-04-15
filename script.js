3aab0fe6bc7ad82bad6f11cb59ffab32f4257da2
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-15 16:35:41 +0200

    Modificato il file script.js: - creata una funzione che fa richiesta api e stampa in console una singola mail.

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // Collego dal dom l'elemento tramite id
+
 const lista = document.querySelector("#lista-email");
+
+// Creo la funzione per fare richiesta API, then1 funzione che mi restituisce un risultato
+// e lo trasforma in json (ho un oggetto con 2 proprietà success: ture e response: mail), 
+// then2 funzione che stampa in console una singola mail (dato.response).
+
+function richiestaApi() {
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+
+    .then(function(risposta) {
+        return risposta.json();
+    })
+    .then(function(dato) {
+        console.log(dato.response)
+    })
+};
+
+richiestaApi();
